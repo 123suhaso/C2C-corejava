0d14d75887db28f78e10899b2ce2fcddfc5f9e04
spoorthio <spoorthiospoorthi204@gmail.com>
2024-03-19 21:48:48 +0530

    First Commit

---
+++ C2CCoreJava/src/IfDemo.java
@@ -0,0 +1,14 @@
+
+
+public class IfDemo {
+	public static void main(String args[]) {
+		int x = 9;
+		if (x!=5)
+			System.out.println("value of x is 5");
+		
+		System.out.println("value of x not a is 5");
+
+		
+		
+	}
+}
